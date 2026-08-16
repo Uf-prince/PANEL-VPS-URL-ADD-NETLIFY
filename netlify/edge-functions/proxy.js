502f9ba2a5ce811f4c7298ea6fabe0ad046dc5fd
UMAR • FAROOQ <ufprince1@gmail.com>
2026-08-16 12:03:48 +0500

    Update proxy.js

--- netlify/edge-functions/proxy.js
+++ netlify/edge-functions/proxy.js
@@ -1,7 +1,7 @@
 // Netlify Edge Function - reverse proxy to the VPS backend
 // Converted from the original Vercel api/index.js (Node http/https proxy)
 
-const TARGET = "http://143.198.88.152:10007";
+const TARGET = "http://node8.xzyx.qzz.io:23469/";
 
 export default async (request, context) => {
   const url = new URL(request.url);

df0f33fca5cb2e91bde305d7462022be2e4ad7cf
UMAR • FAROOQ <ufprince1@gmail.com>
2026-08-16 13:47:58 +0500

    Create bot-link.js

---
+++ bot-link.js
@@ -0,0 +1,5 @@
+// bot-link.js
+// Yahan sirf apna bot ka link change karo. Poori site automatically
+// yahi link use karegi (link box + copy button dono).
+
+const BOT_LINK = "https://node1.xzyx.qzz.io:1257xx";

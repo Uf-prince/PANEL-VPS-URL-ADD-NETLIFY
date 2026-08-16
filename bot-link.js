1d38489e40af32dd44384ac2d5a8b1a34339aaaa
UMAR • FAROOQ <ufprince1@gmail.com>
2026-08-16 16:25:55 +0500

    Update bot-link.js

--- bot-link.js
+++ bot-link.js
@@ -2,4 +2,4 @@
 // Yahan sirf apna bot ka link change karo. Poori site automatically
 // yahi link use karegi (link box + copy button dono).
 
-const BOT_LINK = "https://node1.xzyx.qzz.io:1257xx";
+const BOT_LINK = "https://node8.xzyx.qzz.io:23469";

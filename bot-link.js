ef8cf976f8ba757e9c42b60f64b11a280f431340
UMAR • FAROOQ <ufprince1@gmail.com>
2026-08-16 16:36:14 +0500

    Delete bot-link.js

--- bot-link.js
+++
@@ -1,5 +0,0 @@
-// bot-link.js
-// Yahan sirf apna bot ka link change karo. Poori site automatically
-// yahi link use karegi (link box + copy button dono).
-
-const BOT_LINK = "https://node8.xzyx.qzz.io:23469";
